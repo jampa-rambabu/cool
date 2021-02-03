f03f0214a3e5c6db6fc14224f8df1bec2ecc0c5d
jampa-rambabu <jampa.rambabu8@gmail.com>
2021-02-03 14:32:14 +0530

    cool

---
+++ src/test/java/com/java/DevOps/DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.DevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
